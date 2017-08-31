d1e7b2d27d326450883dc0256b6566fb770c036e
aleksandrprendota <prendota@mail.ru>
2017-08-31 23:50:32 +0300

    Happy Birthday. Project for J. Thanks.

--- src/main/java/com/hifriends/HifriendsApplication.java
+++ src/main/java/com/hifriends/HifriendsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HifriendsApplication {
+    // 31 August Happy Birthday Alex. Project for J. Thanks.
     public static void main(String[] args) {
         SpringApplication.run(HifriendsApplication.class, args);
     }
